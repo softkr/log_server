9fd024433968e54f93a6027450e0a8a2d3d0dd51
Elijah Roh <softkr@kakao.com>
2021-09-14 17:29:25 +0900

    업데이트

--- app.go
+++ app.go
@@ -2,31 +2,31 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
+	log_server_port := os.Getenv("LOG_SERVER_PORT")
 	router := gin.Default()
 	// Set a lower memory limit for multipart forms (default is 32 MiB)
 	router.MaxMultipartMemory = 8 << 20 // 8 MiB
 	router.POST("/", func(c *gin.Context) {
+		guid := c.PostForm("guid")
 		// log 폴더 존재 하지않을경우 생성
 		_, err := os.Stat("./log")
-		fmt.Println(err)
 		if err != nil {
 			os.Mkdir("./log", os.ModePerm)
 		}
 		// single file
 		file, _ := c.FormFile("file")
-		log.Println(file.Filename)
 		// Upload the file to specific dst.
-		c.SaveUploadedFile(file, "./log/"+file.Filename)
-
+		c.SaveUploadedFile(file, fmt.Sprintf("./log/%v_%v", guid, file.Filename))
+		// 응답
 		c.String(http.StatusCreated, fmt.Sprintf("'%s' uploaded!", file.Filename))
 	})
-	router.Run(":9090")
+	router.Run(log_server_port)
 }
